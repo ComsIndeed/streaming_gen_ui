059976d8fa0e045490cf973f8e333ec9662bfcb7
Coms <125128463+ComsIndeed@users.noreply.github.com>
2026-05-19 23:06:11 +0800

    Added first registered widget

--- lib/src/widgets/registries/core_registry.dart
+++ lib/src/widgets/registries/core_registry.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/widgets.dart';
 import 'package:llm_json_stream/llm_json_stream.dart';
+import 'package:streaming_gen_ui/src/widgets/accumulating_string_stream_builder.dart';
 
 final Map<String, Widget Function(BuildContext context, PropertyStream props)>
-coreRegistry = {};
+coreRegistry = {
+  "core:text": (context, props) {
+    final textStream = props.asMap.getStringProperty("content");
+    return AccumulatingStringStreamBuilder(
+      stream: textStream.stream,
+      builder: (context, accumulatedText) => Text(accumulatedText),
+    );
+  },
+};
--- lib/streaming_gen_ui.dart
+++ lib/streaming_gen_ui.dart
@@ -2,3 +2,4 @@ export 'src/streaming_generative_ui.dart';
 export 'src/models/widget_registry.dart';
 export 'src/models/view_state.dart';
 export 'src/widgets/accumulating_string_stream_builder.dart';
+export 'src/widgets/registries/registries.dart';
