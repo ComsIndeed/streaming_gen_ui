c3b395500e3ec5517b7116ea271d412538147fdb
Coms <125128463+ComsIndeed@users.noreply.github.com>
2026-05-19 20:23:12 +0800

    Initial main class

---
+++ lib/src/models/widget_registry.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+import 'package:llm_json_stream/llm_json_stream.dart';
+
+class WidgetRegistry {
+  Map<String, Widget Function(BuildContext context, PropertyStream props)>
+  widgets;
+
+  WidgetRegistry({required this.widgets});
+
+  String get systemPromptFragment => "";
+}
--- lib/src/streaming_generative_ui.dart
+++ lib/src/streaming_generative_ui.dart
@@ -1,1 +1,18 @@
-class StreamingGenerativeUi {}
+import 'package:streaming_gen_ui/src/models/widget_registry.dart';
+
+class StreamingGenerativeUi {
+  final WidgetRegistry registry;
+
+  StreamingGenerativeUi({required this.registry});
+
+  // Input
+  Future<void> stream(Stream<String> stream, {required String viewId}) async {}
+
+  void restore(String content, {required String viewId}) =>
+      stream(Stream.value(content), viewId: viewId);
+
+  // Output
+  void view(String viewId) {}
+
+  void disposeView(String viewId) {}
+}
